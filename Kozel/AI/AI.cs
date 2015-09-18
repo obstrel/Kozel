2337c574acbd1be30a18f55676df79ca1ae1940a
Serge <sergey.xi@yandex.ru>
2015-09-18 20:28:23 +0300

    Начало AI

---
+++ Kozel/AI/AI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kozel.AI {
+    public class AI {
+        public AI() : base() {
+
+        }
+    }
+}
