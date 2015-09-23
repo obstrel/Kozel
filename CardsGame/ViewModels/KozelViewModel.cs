98b1fcdfba889926b83030721a8f8333f357948b
Serge <sergey.xi@yandex.ru>
2015-09-23 21:16:53 +0300

    Начал переделывать под MVVM

---
+++ CardsGame/ViewModels/KozelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsGame.ViewModels {
+    public class KozelViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title { get { return "Kozel Game"; } }
+    }
+}
