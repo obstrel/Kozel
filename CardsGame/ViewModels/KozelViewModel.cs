f8c57132dbae2551cae88b1b78c3ca74d9ef1234
Serge <sergey.xi@yandex.ru>
2015-09-25 19:54:09 +0300

    Продолжение очень серьезного рефакторинга под MVVM

--- CardsGame/ViewModels/KozelViewModel.cs
+++ CardsGame/ViewModels/KozelViewModel.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kozel;
+using System.Windows.Input;
+using CardsGame.Commands;
 
 namespace CardsGame.ViewModels {
     public class KozelViewModel : INotifyPropertyChanged {
@@ -12,11 +14,30 @@ namespace CardsGame.ViewModels {
 
         public string Title { get { return Kozel.KozelGame.Name; } }
         public Player Player1 { get { return kozelGame.Players[0]; } }
+        public Player Player2 { get { return kozelGame.Players[1]; } }
+        public Player Player3 { get { return kozelGame.Players[2]; } }
+        public Player Player4 { get { return kozelGame.Players[3]; } }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        //       private DelegateCommand startGameCommand;
+
         public KozelViewModel() {
             kozelGame = new Kozel.KozelGame();
+        }
+
+        private ICommand newGame;
+        public ICommand NewGame {
+            get {
+                if (newGame == null) {
+                    newGame = new DelegateCommand(
+                        delegate () {
+                            kozelGame.Start();
+                        }
+                    );
+                }
+                return newGame;
+            }
         }
 
     }
